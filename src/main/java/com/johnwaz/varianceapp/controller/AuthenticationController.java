61a4b2d3e02133a8c7d136a676905a7b55d2f712
John Wasielewicz <wazjohn@yahoo.com>
2021-02-10 14:11:15 -0600

    create authentication controller and form dtos

---
+++ src/main/java/com/johnwaz/varianceapp/controller/AuthenticationController.java
@@ -0,0 +1,37 @@
+package com.johnwaz.varianceapp.controller;
+
+import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+}
