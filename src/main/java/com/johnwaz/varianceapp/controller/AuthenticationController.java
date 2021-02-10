830f6fc2c66608da7fd6b1baf5e1bf422e11da19
John Wasielewicz <wazjohn@yahoo.com>
2021-02-10 15:51:33 -0600

    create login form

--- src/main/java/com/johnwaz/varianceapp/controller/AuthenticationController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.johnwaz.varianceapp.controller;
 
 import com.johnwaz.varianceapp.data.UserRepository;
 import com.johnwaz.varianceapp.models.User;
+import com.johnwaz.varianceapp.models.dto.LoginFormDTO;
 import com.johnwaz.varianceapp.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -79,6 +80,44 @@ public class AuthenticationController {
         User newUser = new User(registerFormDTO.getUsername(), registerFormDTO.getPassword());
         userRepository.save(newUser);
         setUserInSession(request.getSession(), newUser);
+
+        return "redirect:";
+    }
+
+    @GetMapping("/login")
+    public String displayLoginForm(Model model) {
+        model.addAttribute(new LoginFormDTO());
+        model.addAttribute("title", "Log In");
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String processLoginForm(@ModelAttribute @Valid LoginFormDTO loginFormDTO,
+                                   Errors errors, HttpServletRequest request,
+                                   Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        User theUser = userRepository.findByUsername(loginFormDTO.getUsername());
+
+        if (theUser == null) {
+            errors.rejectValue("username", "user.invalid", "The given username does not exist");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        String password = loginFormDTO.getPassword();
+
+        if (!theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password", "password.invalid", "Invalid password");
+            model.addAttribute("title", "Log In");
+            return "login";
+        }
+
+        setUserInSession(request.getSession(), theUser);
 
         return "redirect:";
     }
