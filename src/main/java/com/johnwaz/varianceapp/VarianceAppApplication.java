518a6f8d08094ac3d416e6c443c1a78ce6064b64
John Wasielewicz <wazjohn@yahoo.com>
2021-02-10 09:56:50 -0600

    initial commit

---
+++ src/main/java/com/johnwaz/varianceapp/VarianceAppApplication.java
@@ -0,0 +1,13 @@
+package com.johnwaz.varianceapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VarianceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VarianceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/johnwaz/varianceapp/VarianceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.johnwaz.varianceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarianceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
