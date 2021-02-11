33ed2ce5e24d3e6b858f77e5a614a241864501fd
John Wasielewicz <wazjohn@yahoo.com>
2021-02-11 09:10:50 -0600

    modify book controller books index and fragments

--- src/main/java/com/johnwaz/varianceapp/controller/BookController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/BookController.java
@@ -2,9 +2,15 @@ package com.johnwaz.varianceapp.controller;
 
 import com.johnwaz.varianceapp.data.BookRepository;
 import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @Controller
 @RequestMapping("books")
@@ -15,4 +21,15 @@ public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
+
+    private static final String userSessionKey = "user";
+
+    @GetMapping
+    public String displayAllBooks(Model model, HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        model.addAttribute("user", user);
+        model.addAttribute("books", bookRepository.findAllById(Collections.singleton(userId)));
+        return "books/index";
+    }
 }
