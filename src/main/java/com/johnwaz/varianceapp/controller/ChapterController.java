4b40ddfe7697b76c2c86f54b9f82c1811ccfe5a2
John Wasielewicz <wazjohn@yahoo.com>
2021-02-11 13:48:43 -0600

    create display add chapter form and process add chapter form

--- src/main/java/com/johnwaz/varianceapp/controller/ChapterController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/ChapterController.java
@@ -3,15 +3,20 @@ package com.johnwaz.varianceapp.controller;
 import com.johnwaz.varianceapp.data.BookRepository;
 import com.johnwaz.varianceapp.data.ChapterRepository;
 import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.Book;
+import com.johnwaz.varianceapp.models.Chapter;
 import com.johnwaz.varianceapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.Collections;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("chapters")
@@ -27,4 +32,47 @@ public class ChapterController {
     private ChapterRepository chapterRepository;
 
     private static final String userSessionKey = "user";
+
+    @GetMapping(path = {"add/{bookId}", "add"})
+    public String displayAddChapterForm(Model model, @PathVariable(required = false) Integer bookId, HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        if (bookId == null) {
+            model.addAttribute("user", user);
+            model.addAttribute("chapters", chapterRepository.findAllById(Collections.singleton(userId)));
+            return "redirect:../";
+        } else {
+            Optional<Book> result = bookRepository.findById(bookId);
+            if (result.isEmpty()) {
+                return "redirect:../";
+            } else {
+                Book book = result.get();
+                if (user.getId() != book.getUser().getId()) {
+                    return "redirect:../";
+                }
+                model.addAttribute(new Chapter());
+                model.addAttribute("book", book);
+            }
+        }
+        return "chapters/add";
+    }
+
+    @PostMapping("add/{bookId}")
+    public String processAddChapterForm(@Valid @ModelAttribute Chapter newChapter,
+                                       Errors errors, Model model, @PathVariable int bookId,
+                                       HttpSession session, RedirectAttributes redirectAttributes) {
+        if (errors.hasErrors()) {
+            model.addAttribute("book", bookRepository.findById(bookId).get());
+            return "devices/add";
+        }
+        Optional optBook = bookRepository.findById(bookId);
+        Book book = bookRepository.findById(bookId).get();
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        redirectAttributes.addAttribute("id", optBook.get());
+        newChapter.setUser(user);
+        newChapter.setBook(book);
+        chapterRepository.save(newChapter);
+        return "redirect:/books/view/{id}";
+    }
 }
