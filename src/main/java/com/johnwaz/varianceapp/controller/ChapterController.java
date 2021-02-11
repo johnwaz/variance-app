b58e362ecb6d110b312d45ab38b31c152b71b2dd
John Wasielewicz <wazjohn@yahoo.com>
2021-02-11 13:37:54 -0600

    add display all chapters to chapter controller

--- src/main/java/com/johnwaz/varianceapp/controller/ChapterController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/ChapterController.java
@@ -3,10 +3,18 @@ package com.johnwaz.varianceapp.controller;
 import com.johnwaz.varianceapp.data.BookRepository;
 import com.johnwaz.varianceapp.data.ChapterRepository;
 import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @Controller
+@RequestMapping("chapters")
 public class ChapterController {
 
     @Autowired
@@ -17,4 +25,15 @@ public class ChapterController {
 
     @Autowired
     private ChapterRepository chapterRepository;
+
+    private static final String userSessionKey = "user";
+
+    @GetMapping
+    public String displayAllChapters(Model model, HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        model.addAttribute("user", user);
+        model.addAttribute("chapters", chapterRepository.findAllById(Collections.singleton(userId)));
+        return "chapters/index";
+    }
 }
