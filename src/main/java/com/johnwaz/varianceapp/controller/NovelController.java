23d10a774898efe27b22586cb3b0f8f57745c345
John Wasielewicz <wazjohn@yahoo.com>
2021-02-14 17:05:47 -0600

    update novel controller

--- src/main/java/com/johnwaz/varianceapp/controller/NovelController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/NovelController.java
@@ -1,9 +1,35 @@
 package com.johnwaz.varianceapp.controller;
 
+import com.johnwaz.varianceapp.data.NovelRepository;
+import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
 
 @Controller
 @RequestMapping("novels")
 public class NovelController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private NovelRepository novelRepository;
+
+    private static final String userSessionKey = "user";
+
+    @GetMapping
+    public String displayAllNovels(Model model, HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        model.addAttribute("user", user);
+        model.addAttribute("novels", novelRepository.findAllById(Collections.singleton(userId)));
+        return "novels/index";
+    }
 }
