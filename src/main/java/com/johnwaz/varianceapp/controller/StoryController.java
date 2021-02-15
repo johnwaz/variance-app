9514f4b7ef68be246e1618ec5479e0f0f4dbaeba
John Wasielewicz <wazjohn@yahoo.com>
2021-02-15 15:01:29 -0600

    create display add story to novel form and process add story to novel form

--- src/main/java/com/johnwaz/varianceapp/controller/StoryController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/StoryController.java
@@ -3,9 +3,18 @@ package com.johnwaz.varianceapp.controller;
 import com.johnwaz.varianceapp.data.NovelRepository;
 import com.johnwaz.varianceapp.data.StoryRepository;
 import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Collections;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("stories")
@@ -21,4 +30,47 @@ public class StoryController {
     private StoryRepository storyRepository;
 
     private static final String userSessionKey = "user";
+
+    @GetMapping(path = {"add/{novelId}", "add"})
+    public String displayAddStoryToNovelForm(Model model, @PathVariable(required = false) Integer novelId, HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        if (novelId == null) {
+            model.addAttribute("user", user);
+            model.addAttribute("stories", storyRepository.findAllById(Collections.singleton(userId)));
+            return "stories/index";
+        } else {
+            Optional<Novel> result = novelRepository.findById(novelId);
+            if (result.isEmpty()) {
+                return "stories/index";
+            } else {
+                Novel novel = result.get();
+                if (user.getId() != novel.getUser().getId()) {
+                    return "stories/index";
+                }
+                model.addAttribute(new Story());
+                model.addAttribute("novel", novel);
+            }
+        }
+        return "stories/add";
+    }
+
+    @PostMapping("add/{novelId}")
+    public String processAddStoryToNovelForm(@Valid @ModelAttribute Story newStory,
+                                              Errors errors, Model model, @PathVariable int novelId,
+                                              HttpSession session, RedirectAttributes redirectAttributes) {
+        if (errors.hasErrors()) {
+            model.addAttribute("novel", novelRepository.findById(novelId).get());
+            return "stories/add";
+        }
+        Optional optNovel = novelRepository.findById(novelId);
+        Novel novel = novelRepository.findById(novelId).get();
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        redirectAttributes.addAttribute("id", optNovel.get());
+        newStory.setUser(user);
+        newStory.setNovel(novel);
+        storyRepository.save(newStory);
+        return "redirect:/novels/view/{id}";
+    }
 }
