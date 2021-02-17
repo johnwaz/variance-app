d942f801d7d70e275241a23dd3850075b3317885
John Wasielewicz <wazjohn@yahoo.com>
2021-02-17 10:21:46 -0600

    update journal controller

--- src/main/java/com/johnwaz/varianceapp/controller/JournalController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/JournalController.java
@@ -1,9 +1,20 @@
 package com.johnwaz.varianceapp.controller;
 
+import com.johnwaz.varianceapp.data.JournalRepository;
+import com.johnwaz.varianceapp.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("journals")
 public class JournalController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JournalRepository journalRepository;
+
+    private static final String userSessionKey = "user";
 }
