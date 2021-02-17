8f78213c6763e0b74536126ac40417d17767cd28
John Wasielewicz <wazjohn@yahoo.com>
2021-02-17 10:17:48 -0600

    create journal controller

---
+++ src/main/java/com/johnwaz/varianceapp/controller/JournalController.java
@@ -0,0 +1,9 @@
+package com.johnwaz.varianceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("journals")
+public class JournalController {
+}
