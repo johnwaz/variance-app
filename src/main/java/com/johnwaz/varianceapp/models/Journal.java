8f63c556cde951b448e04de785794501e3eeb714
John Wasielewicz <wazjohn@yahoo.com>
2021-02-17 10:13:09 -0600

    update journal model

--- src/main/java/com/johnwaz/varianceapp/models/Journal.java
+++ src/main/java/com/johnwaz/varianceapp/models/Journal.java
@@ -1,11 +1,62 @@
 package com.johnwaz.varianceapp.models;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Journal extends AbstractEntity {
 
     @ManyToOne
     private User user;
+
+    @OneToMany(mappedBy = "journal", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Page> pages = new ArrayList<>();
+
+    @NotBlank(message = "Please give the journal a name")
+    private String name;
+
+    @Size(max = 250, message = "Description must be less than 250 characters")
+    private String description;
+
+    public Journal(User user, @NotBlank String name, @Size(max = 250, message = "Description must be less than 250 characters") String description) {
+        this.user = user;
+        this.name = name;
+        this.description = description;
+    }
+
+    public Journal() {}
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Page> getPages() {
+        return pages;
+    }
 }
