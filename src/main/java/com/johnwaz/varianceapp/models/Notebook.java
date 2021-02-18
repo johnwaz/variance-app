4041c7c3b7a241920735f21a2319a3ba85ace81b
John Wasielewicz <wazjohn@yahoo.com>
2021-02-18 14:10:55 -0600

    update notebook model

--- src/main/java/com/johnwaz/varianceapp/models/Notebook.java
+++ src/main/java/com/johnwaz/varianceapp/models/Notebook.java
@@ -1,7 +1,11 @@
 package com.johnwaz.varianceapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Notebook extends AbstractEntity {
+
+    @ManyToOne
+    private User user;
 }
