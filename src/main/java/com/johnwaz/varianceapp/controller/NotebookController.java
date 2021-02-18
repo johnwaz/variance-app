78aca512b7700ce975077087834928c8dce1aa00
John Wasielewicz <wazjohn@yahoo.com>
2021-02-18 15:31:31 -0600

    create display all notebooks display add notebook form and process add notebook form

--- src/main/java/com/johnwaz/varianceapp/controller/NotebookController.java
+++ src/main/java/com/johnwaz/varianceapp/controller/NotebookController.java
@@ -2,9 +2,21 @@ package com.johnwaz.varianceapp.controller;
 
 import com.johnwaz.varianceapp.data.NotebookRepository;
 import com.johnwaz.varianceapp.data.UserRepository;
+import com.johnwaz.varianceapp.models.Notebook;
+import com.johnwaz.varianceapp.models.Novel;
+import com.johnwaz.varianceapp.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+import java.util.Collections;
 
 @Controller
 @RequestMapping("notebooks")
@@ -17,4 +29,33 @@ public class NotebookController {
     private NotebookRepository notebookRepository;
 
     private static final String userSessionKey = "user";
+
+    @GetMapping
+    public String displayAllNotebooks(Model model, HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        model.addAttribute("user", user);
+        model.addAttribute("notebooks", notebookRepository.findAllById(Collections.singleton(userId)));
+        return "notebooks/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddNotebookForm(Model model) {
+        model.addAttribute(new Notebook());
+        model.addAttribute("notebooks", notebookRepository.findAll());
+        return "notebooks/add";
+    }
+
+    @PostMapping("add")
+    public String processAddNotebookForm(@Valid @ModelAttribute Notebook newNotebook,
+                                      Errors errors, HttpSession session) {
+        if (errors.hasErrors()) {
+            return "notebooks/add";
+        }
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).get();
+        newNotebook.setUser(user);
+        notebookRepository.save(newNotebook);
+        return "redirect:";
+    }
 }
