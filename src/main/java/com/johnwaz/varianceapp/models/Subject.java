e14f793cf1011e64d63e614286a9ddb4cde17148
John Wasielewicz <wazjohn@yahoo.com>
2021-02-18 14:14:06 -0600

    update subject model

--- src/main/java/com/johnwaz/varianceapp/models/Subject.java
+++ src/main/java/com/johnwaz/varianceapp/models/Subject.java
@@ -1,7 +1,11 @@
 package com.johnwaz.varianceapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Subject extends AbstractEntity {
+
+    @ManyToOne
+    private User user;
 }
